6fd8069f3c29f85c600434826350121e52d10f59
joao monteiro <joaoagrom@gmail.com>
2018-11-30 00:45:29 -0200

    using XGBRegressor not utilizing Id column to test using all data

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pandas as pd
 
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
+from xgboost import XGBRegressor
 
-train_data = pd.read_csv('train.csv', sep=',')
-test_data = pd.read_csv('test.csv', sep=',')
+test_data = pd.read_csv('test.csv')
+test_X = test_data.select_dtypes(exclude=['object'])
+test_X = test_X.drop(['Id'], axis=1)
 
-y = train_data.SalePrice
-
-X = train_data.drop(["SalePrice"], axis=1)
-numeric_X = X.select_dtypes(exclude=['object'])
-
-train_X, test_X, train_y, test_y = train_test_split(numeric_X, y, random_state=1)
+train_data = pd.read_csv('train.csv')
+train_y = train_data.SalePrice
+train_X= train_data.drop(['SalePrice'], axis=1)
+train_X = train_X.select_dtypes(exclude=['object'])
+train_X = train_X.drop(['Id'], axis=1)
 
 my_imputer = SimpleImputer()
 
-imputed_train_X = my_imputer.fit_transform(train_X)
-imputed_test_X = my_imputer.transform(test_X)
+train_X = my_imputer.fit_transform(train_X)
+test_X = my_imputer.transform(test_X)
 
-model = RandomForestRegressor(random_state=1)
-model.fit(imputed_train_X, train_y)
+model = XGBRegressor(n_estimators=7950, learning_rate=0.0005, n_jobs=4)
+model.fit(train_X, train_y)
 
-predictions = model.predict(imputed_test_X)
-mae = mean_absolute_error(test_y, predictions)
+predictions = model.predict(test_X)
 
-print(mae)
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': predictions})
+
+output.to_csv('submission_1.csv', index=False)
