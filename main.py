d38c2033a3b83bd01e7efa0b612dfd7e56e914ea
joao monteiro <joaoagrom@gmail.com>
2018-12-02 23:57:35 -0200

    using pipeline changed XGB parameters

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+
 from xgboost import XGBRegressor
 
 test_data = pd.read_csv('test.csv')
-test_X = test_data.select_dtypes(exclude=['object'])
-test_X = test_X.drop(['Id'], axis=1)
+X_test_final = test_data.drop(['Id'], axis=1)
 
 train_data = pd.read_csv('train.csv')
+y_train_final = train_data.SalePrice
+X_train_final = train_data.drop(['Id', 'SalePrice'], axis=1)
 
-train_y = train_data.SalePrice
 
-train_X = train_data.drop(['SalePrice'], axis=1)
-train_X = train_X.select_dtypes(exclude=['object'])
-train_X = train_X.drop(['Id'], axis=1)
+X_train_final = pd.get_dummies(X_train_final)
+X_test_final = pd.get_dummies(X_test_final)
 
-my_imputer = SimpleImputer()
+X_train_final, X_test_final = X_train_final.align(X_test_final, join='left', axis=1)
 
-train_X = my_imputer.fit_transform(train_X)
-test_X = my_imputer.transform(test_X)
+cols_with_missing_values = [col for col in X_train_final.columns
+                            if X_train_final[col].isnull().any()]
 
-model = XGBRegressor(n_estimators=7950, learning_rate=0.0005, n_jobs=4)
-model.fit(train_X, train_y)
+for col in cols_with_missing_values:
+    X_train_final[col + '_was_missing'] = X_train_final[col].isnull()
+    X_test_final[col + '_was_missing'] = X_test_final[col].isnull()
 
-predictions = model.predict(test_X)
+pipeline = make_pipeline(SimpleImputer(), XGBRegressor(n_estimators=1000, learning_rate=0.065))
 
-# output = pd.DataFrame({'Id': test_data.Id,
-#                        'SalePrice': predictions})
-#
-# output.to_csv('submission_1.csv', index=False)
+
+pipeline.fit(X_train_final, y_train_final)
+
+predictions = pipeline.predict(X_test_final)
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': predictions})
+
+output.to_csv('submission_1.csv', index=False)
